718d69a14e22dcbcb3e4cdee7640d3a78a722f4f
Victor Chelaru <VicChelaru@gmail.com>
2017-06-07 21:12:17 -0600

    First pass on bouncing ball.

--- Dodgeball/Dodgeball/Entities/Ball.cs
+++ Dodgeball/Dodgeball/Entities/Ball.cs
@@ -13,6 +13,8 @@ namespace Dodgeball.Entities
 {
 	public partial class Ball
 	{
+        #region Enums
+
         public enum OwnershipState
         {
             Free,
@@ -20,12 +22,20 @@ namespace Dodgeball.Entities
             Held
         }
 
+        #endregion
+
+        #region Fields / Properties
+
+        float ballAltitude;
+        float altitudeVelocity = 0;
+
         public int OwnerTeam { get; set; }
 
         public Player ThrowOwner { get; set; }
 
         public OwnershipState CurrentOwnershipState { get; set; }
 
+        #endregion
 
 
         /// <summary>
@@ -33,19 +43,46 @@ namespace Dodgeball.Entities
         /// This method is called when the Entity is added to managers. Entities which are instantiated but not
         /// added to managers will not have this method called.
         /// </summary>
-		private void CustomInitialize()
+        private void CustomInitialize()
 		{
-
+            ballAltitude = this.HeightWhenThrown;
 
 		}
 
 		private void CustomActivity()
 		{
-
-
+            PerformFallingAndBouncingActivity();
 		}
 
-		private void CustomDestroy()
+        private void PerformFallingAndBouncingActivity()
+        {
+            bool shouldFallAndBounce = this.CurrentOwnershipState != OwnershipState.Held;
+
+            if(shouldFallAndBounce)
+            {
+                // linear approx. is fine:
+                altitudeVelocity += TimeManager.SecondDifference * -BallGravity;
+                ballAltitude += TimeManager.SecondDifference * altitudeVelocity;
+            }
+            else
+            {
+                ballAltitude = HeightWhenThrown;
+                altitudeVelocity = 0;
+            }
+
+            this.SpriteInstance.RelativeY = ballAltitude + SpriteInstance.Height / 2.0f;
+
+            if(SpriteInstance.RelativeBottom < 0 && altitudeVelocity < 0)
+            {
+                // move it above the ground 
+                SpriteInstance.RelativeBottom = 0;
+
+                // make it bounce, but lose some height
+                altitudeVelocity *= -.95f;
+            }
+        }
+
+        private void CustomDestroy()
 		{
 
 
