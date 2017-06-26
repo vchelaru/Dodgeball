23cb9ef9fd6469cd97878d051b46649f89816736
Victor Chelaru <VicChelaru@gmail.com>
2017-06-25 20:35:18 -0600

    Fixed the character select screen not matching up with xbox controllers properly.

--- Dodgeball/Dodgeball/Screens/CharacterSelectScreen.cs
+++ Dodgeball/Dodgeball/Screens/CharacterSelectScreen.cs
@@ -18,14 +18,20 @@ namespace Dodgeball.Screens
     public partial class CharacterSelectScreen
     {
         enum JoinStatus { Team1, Undecided, Team2 };
-        private JoinStatus Player1JoinStatus = JoinStatus.Undecided;
-        private JoinStatus Player2JoinStatus = JoinStatus.Undecided;
-        private JoinStatus Player3JoinStatus = JoinStatus.Undecided;
-        private JoinStatus Player4JoinStatus = JoinStatus.Undecided;
+
+        private JoinStatus[] JoinStatuses;
 
 
         void CustomInitialize()
         {
+            JoinStatuses = new JoinStatus[]
+            {
+                JoinStatus.Undecided,
+                JoinStatus.Undecided,
+                JoinStatus.Undecided,
+                JoinStatus.Undecided
+            };
+
             InitializeInput();
 
         }
@@ -36,50 +42,52 @@ namespace Dodgeball.Screens
 
         void CustomActivity(bool firstTimeCalled)
         {
-            var player1Marker = TeamSelectionBoxesInstance.Children.Find(item => item.Name == "Player1Marker") as GraphicalUiElement;
-            var player2Marker = TeamSelectionBoxesInstance.Children.Find(item => item.Name == "Player2Marker") as GraphicalUiElement;
-            var player3Marker = TeamSelectionBoxesInstance.Children.Find(item => item.Name == "Player3Marker") as GraphicalUiElement;
-            var player4Marker = TeamSelectionBoxesInstance.Children.Find(item => item.Name == "Player4Marker") as GraphicalUiElement;
-            if (InputManager.Xbox360GamePads[0].IsConnected)
+            var markers = TeamSelectionBoxesInstance.Children
+                .Where(item => item.Name.StartsWith("Player") && item.Name.EndsWith("Marker"))
+                .OrderBy(item => item.Name)
+                .Select(item => item as GraphicalUiElement)
+                .ToList();
+
+            for(int i = 0; i < 4; i++)
             {
-                HandleGamePadInput(InputManager.Xbox360GamePads[0].LeftStick.RightAsButton, InputManager.Xbox360GamePads[0].LeftStick.LeftAsButton, ref Player1JoinStatus, player1Marker);
-                player1Marker.Visible = true;
+                var gamepad = InputManager.Xbox360GamePads[i];
+                markers[i].Visible = gamepad.IsConnected;
+
+                if (InputManager.Xbox360GamePads[i].IsConnected)
+                {
+                    HandleGamePadInput(gamepad.LeftStick.RightAsButton, gamepad.LeftStick.LeftAsButton, ref JoinStatuses[i], markers[i]);
+                }
             }
-            else { player1Marker.Visible = false; }
-            if (InputManager.Xbox360GamePads[1].IsConnected)
-            {
-                HandleGamePadInput(InputManager.Xbox360GamePads[1].LeftStick.RightAsButton, InputManager.Xbox360GamePads[1].LeftStick.LeftAsButton, ref Player2JoinStatus, player2Marker);
-                player2Marker.Visible = true;
-            }
-            else { player1Marker.Visible = false; }
-            if (InputManager.Xbox360GamePads[2].IsConnected)
-            {
-                HandleGamePadInput(InputManager.Xbox360GamePads[2].LeftStick.RightAsButton, InputManager.Xbox360GamePads[2].LeftStick.LeftAsButton, ref Player3JoinStatus, player3Marker);
-                player3Marker.Visible = true;
-            }
-            else { player3Marker.Visible = false; }
-            if (InputManager.Xbox360GamePads[3].IsConnected)
-            {
-                HandleGamePadInput(InputManager.Xbox360GamePads[3].LeftStick.RightAsButton, InputManager.Xbox360GamePads[3].LeftStick.LeftAsButton, ref Player4JoinStatus, player4Marker);
-                player4Marker.Visible = true;
-            }
-            else { player4Marker.Visible = false; }
+            
             foreach (Xbox360GamePad gamePad in InputManager.Xbox360GamePads)
             {
                 if (gamePad.ButtonPushed(Xbox360GamePad.Button.A))
                 {
-                    if ((Player1JoinStatus != JoinStatus.Undecided || InputManager.Xbox360GamePads[0].IsConnected == false) &&
-                        (Player2JoinStatus != JoinStatus.Undecided || InputManager.Xbox360GamePads[1].IsConnected == false) &&
-                        (Player3JoinStatus != JoinStatus.Undecided || InputManager.Xbox360GamePads[2].IsConnected == false) &&
-                        (Player4JoinStatus != JoinStatus.Undecided || InputManager.Xbox360GamePads[3].IsConnected == false))
+                    bool hasAnyUndecided = GetIfHasAnyUndecided();
+
+                    if (hasAnyUndecided == false)
                     {
                         TeamSelectionBoxesInstance.SelectTeamVisible = false;
                         TeamSelectionBoxesInstance.LoadingVisible = true;
                         this.Call(() => MoveToScreen(typeof(GameScreen))).After(0);
                     }
-
                 }
             }
+        }
+
+        private bool GetIfHasAnyUndecided()
+        {
+            bool hasAnyUndecided = false;
+            for (int i = 0; i < 4; i++)
+            {
+                if (JoinStatuses[i] == JoinStatus.Undecided && InputManager.Xbox360GamePads[i].IsConnected)
+                {
+                    hasAnyUndecided = true;
+                    break;
+                }
+            }
+
+            return hasAnyUndecided;
         }
 
         void CustomDestroy()
